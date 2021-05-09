e6e4ba52501c7fd795f6fbae1efcc03641810d69
felmola <felmola@gmail.com>
2021-05-09 11:38:22 -0500

    Created user input function ask_user()

--- rps.py
+++ rps.py
@@ -1,15 +1,38 @@
 """
-#todo: Description goes here
+Implements the game of Rock-Paper-Scissors!
+
+History:
+This classic game dates back to the Han Dinesty, over 2200 years ago.
+The First International Rock-Paper-Scissors Programming Competition
+was held in 1999 and was won by a team called "Iocaine Powder"
+
+The Game:
+Each player choses a move (simultaneously) from the choices:
+rock, paper or scissors.
+If they chose the same move the game is a tie. Otherwise:
+rock beats scissors
+scissors beats paper
+paper beats rock.
+
+In this program a human plays against an AI. The AI choses randomly
+(we promise). The game is repeated N_GAMES times and the human gets
+a total score. Each win is worth +1 points, each loss is worth -1
 """
+#todo: Create input for human
+
 # Imports
 
 
 # Constants
 
-
 def main():
-    pass
+    ask_user()
 
+
+def ask_user():
+    decision = int(input("Please choose a move from Rock(1), Paper(2), or Scissors(3): "))
+    print("User input was:", str(decision))
+    return decision
 
 if __name__ == '__main__':
     main()
