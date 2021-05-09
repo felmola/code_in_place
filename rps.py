883d606c4c0c60a6dfeeb582024515276b722dbe
felmola <felmola@gmail.com>
2021-05-09 11:44:24 -0500

    Created machine input function ask_machine()

--- rps.py
+++ rps.py
@@ -21,18 +21,26 @@ a total score. Each win is worth +1 points, each loss is worth -1
 #todo: Create input for human
 
 # Imports
-
+import random
 
 # Constants
 
 def main():
     ask_user()
+    ask_machine()
 
 
 def ask_user():
-    decision = int(input("Please choose a move from Rock(1), Paper(2), or Scissors(3): "))
-    print("User input was:", str(decision))
-    return decision
+    user_decision = int(input("Please choose a move from Rock(1), Paper(2), or Scissors(3): "))
+    print("User input was:", str(user_decision))
+    return user_decision
+
+
+def ask_machine():
+    machine_decision = random.randint(1, 3)
+    print("Machine input was: ", str(machine_decision))
+    return machine_decision
+
 
 if __name__ == '__main__':
     main()
