9b9b4afe6d242194d3e53b3c127b1f649d0af39c
felmola <felmola@gmail.com>
2021-05-08 20:41:47 -0500

    Introduced a function that depending on the round (even or odd) assigns a player role

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ MAX_STONES = 20
 
 def main():
     stones = MAX_STONES
+    round = 0
     while stones > 0:
         print("There are", str(stones), "stones left.")
+        round += 1
+        player = define_player(round)
         ask_stones = ask_user_stones()
-        print("User asked for", str(ask_stones), "stones.")
         stones = stones - ask_stones
         print()
     print("There are not any more stones.")
@@ -21,10 +23,21 @@ def main():
 
 def ask_user_stones():
     ask_stones = int(input("How many stones would you like to remove?"))
+    print("User asked for", str(ask_stones), "stones.")
     return ask_stones
 
-
-
+def define_player(round):
+    player = None
+    if round % 2 == 0:
+        player = 0
+        print("Round is", str(round))
+        print("Player is", str(player))
+        return player
+    else:
+        player = 1
+        print("Round is", str(round))
+        print("Player is", str(player))
+        return player
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
