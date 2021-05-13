439b8dca3c4d35b64d5b32ece9254f83d0d69567
felmola <felmola@gmail.com>
2021-05-13 16:20:03 -0500

    Created codeinplace.py

---
+++ codeinplace_filter.py
@@ -0,0 +1,20 @@
+"""
+This is the Ancient Game of Nim. There is an initial pile of stones. Players take turns to take 1 or 2 stones from the
+pile. The one that has the last turn before the stones runs out, wins the game.
+"""
+# Installs:
+# python -m pip install PIllow
+
+# Imports:
+# first include simpleimage.py in the project folder
+from simpleimage import SimpleImage
+
+# Constants:
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
